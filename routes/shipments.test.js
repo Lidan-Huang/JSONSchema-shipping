b8874e343b20670eab56e06af6bbf19dd57b2d52
Ryan Nissen <ryankn852@gmail.com>
2022-02-22 18:54:42 -0600

    shipping test complete

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -5,7 +5,20 @@ const app = require("../app");
 
 
 describe("POST /", function () {
-  test("valid", async function () {
+  test("Valid", async function () {
+    const resp = await request(app).post("/shipments").send({
+      shipment: {
+        productId: 1000,
+        name: "Test Tester",
+        addr: "100 Test St",
+        zipcode: "12345-6789"
+      }
+    });
+
+    expect(resp.body).toEqual({ shipped: expect.any(Number) });
+  });
+
+  test("Not valid - No shipment key", async function () {
     const resp = await request(app).post("/shipments").send({
       productId: 1000,
       name: "Test Tester",
@@ -13,6 +26,24 @@ describe("POST /", function () {
       zipcode: "12345-6789",
     });
 
-    expect(resp.body).toEqual({ shipped: expect.any(Number) });
+    expect(resp.body).toEqual({ "error": {"message": "Need key of the 'shipment'", "status": 400} });
+
+    expect(resp.statusCode).toEqual(400);
+  });
+
+  test("Not valid - Zipcode should be string", async function () {
+    const resp = await request(app).post("/shipments").send({
+      shipment: {
+        productId: 800,
+        name: "Test Tester",
+        addr: "100 Test St",
+        zipcode: 12345
+      }
+    });
+
+    expect(resp.body).toEqual({ "error": { "message": 
+        ["instance.productId must be greater than or equal to 1000", 
+        "instance.zipcode is not of a type(s) string"], 
+        "status": 400,} });
   });
 });
